bfdceedcab4c88a85e5579a01681cb15087d4ac7
Pugzy <pugzy@mail.com>
2022-12-17 21:36:16 +0000

    Add whitelist and spectate OP overrides

--- src/main/java/dev/pgm/events/listeners/PlayerJoinListen.java
+++ src/main/java/dev/pgm/events/listeners/PlayerJoinListen.java
@@ -55,10 +55,13 @@ public class PlayerJoinListen implements Listener {
     // check if the player is on one of the teams
     if (playerTeam.isPresent()) {
       Team team = playerTeam.get();
-      if (!isFull(team)) return;
 
-      // team is full -- lets kick this mad lad
-      event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Your team is full!");
+      if (isFull(team)) {
+        // team is full -- lets kick this mad lad
+        event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Your team is full!");
+      } else {
+        event.allow();
+      }
       return;
     }
 
@@ -72,7 +75,8 @@ public class PlayerJoinListen implements Listener {
 
   @EventHandler(priority = EventPriority.HIGH)
   public void vanish(PlayerJoinEvent event) {
-    if (event.getPlayer().hasPermission("events.spectate.vanish")
+    if (!event.getPlayer().isOp()
+        && event.getPlayer().hasPermission("events.spectate.vanish")
         && !manager.playerTeam(event.getPlayer().getUniqueId()).isPresent())
       Integration.setVanished(PGM.get().getMatchManager().getPlayer(event.getPlayer()), true, true);
   }
